b2639275962ee94618eb734330eba45b31a9b76d
AlexisMadridB <jordimadrid52@gmail.com>
2026-05-24 13:33:07 -0500

    Solución npm start

--- src/navbar/navbar.component.ts
+++ src/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { AuthService } from '../../services/auth.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
